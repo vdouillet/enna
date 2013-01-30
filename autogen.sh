249216a2a60d480ab793d191ff116f4f214ec012
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-01-30 13:13:02 +0100

    use autoreconf in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -19,18 +19,5 @@
 # License along with Enna; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-rm -rf autom4te.cache
-rm -f aclocal.m4 ltmain.sh
-
-touch README
-
-echo "Running autopoint..." ; autopoint -f || exit 1
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
-echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
-
-if [ -z "$NOCONFIGURE" ]; then
-	./configure "$@"
-fi
+autoreconf -vif
+./configure $@

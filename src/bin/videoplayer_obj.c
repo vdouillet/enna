c924969b1fd1e3580fa4f526e3b9029d2bb5c8e0
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-02-19 23:10:53 +0100

    Fix indentation

--- src/bin/videoplayer_obj.c
+++ src/bin/videoplayer_obj.c
@@ -64,34 +64,34 @@ int _enna_log_dom_global;
 #define CRIT(...) EINA_LOG_DOM_CRIT(_enna_log_dom_global, __VA_ARGS__)
 
 
-#define PRIV_DATA_GET(o, type, ptr) \
-  type * ptr = evas_object_data_get(o, #type)
+#define PRIV_DATA_GET(o, type, ptr)             \
+    type * ptr = evas_object_data_get(o, #type)
 
-#define PRIV_GET_OR_RETURN(o, type, ptr)           \
-  PRIV_DATA_GET(o, type, ptr);                     \
-  if (!ptr)                                        \
-    {                                              \
-       CRIT("No private data for object %p (%s)!", \
-               o, #type);                          \
-       abort();                                    \
-       return;                                     \
+#define PRIV_GET_OR_RETURN(o, type, ptr)            \
+    PRIV_DATA_GET(o, type, ptr);                    \
+    if (!ptr)                                       \
+    {                                               \
+        CRIT("No private data for object %p (%s)!", \
+             o, #type);                             \
+        abort();                                    \
+        return;                                     \
     }
 
-#define PRIV_GET_OR_RETURN_VAL(o, type, ptr)       \
-  DATA_GET(o, type, ptr);                          \
-  if (!ptr)                                        \
-    {                                              \
-       CRIT("No private data for object %p (%s)!", \
-               o, #type);                          \
-       abort();                                    \
-       return val;                                 \
+#define PRIV_GET_OR_RETURN_VAL(o, type, ptr)        \
+    DATA_GET(o, type, ptr);                         \
+    if (!ptr)                                       \
+    {                                               \
+        CRIT("No private data for object %p (%s)!", \
+             o, #type);                             \
+        abort();                                    \
+        return val;                                 \
     }
 
-#define FREE_NULL(p) \
-        if (p) { free(p); p = NULL; }
+#define FREE_NULL(p)                            \
+    if (p) { free(p); p = NULL; }
 
-#define FREE_NULL_FUNC(fn, p) \
-        if (p) { fn(p); p = NULL; }
+#define FREE_NULL_FUNC(fn, p)                   \
+    if (p) { fn(p); p = NULL; }
 
 #define ENNA_CONFIG_FILE "enna.conf"
 
@@ -197,45 +197,45 @@ static Enna_Config_Section_Parser cfg_mediaplayer = {
 static Eina_Bool
 _osd_timer_cb(void *data)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   if (priv->on_hold)
-   {
-       return EINA_FALSE;
-   }
-   elm_object_signal_emit(priv->layout, "hide,osd", "enna");
-   return EINA_TRUE;
+    if (priv->on_hold)
+    {
+        return EINA_FALSE;
+    }
+    elm_object_signal_emit(priv->layout, "hide,osd", "enna");
+    return EINA_TRUE;
 }
 
 static void
 _set_osd_timer(Enna_View_Player_Video_Data *priv, double t)
 {
-   FREE_NULL_FUNC(ecore_timer_del, priv->osd_timer);
+    FREE_NULL_FUNC(ecore_timer_del, priv->osd_timer);
 
-   if (t >= 0.0)
-     priv->osd_timer = ecore_timer_add(t, _osd_timer_cb, priv);
-   elm_object_signal_emit(priv->layout, "show,osd", "enna");
+    if (t >= 0.0)
+        priv->osd_timer = ecore_timer_add(t, _osd_timer_cb, priv);
+    elm_object_signal_emit(priv->layout, "show,osd", "enna");
 }
 
 void
 _update_time_part(Evas_Object *obj, const char *part, double t)
 {
-   Eina_Strbuf *str;
-   double s;
-   int h, m;
+    Eina_Strbuf *str;
+    double s;
+    int h, m;
 
-   s = t;
-   h = (int)(s / 3600.0);
-   s -= h * 3600;
-   m = (int)(s / 60.0);
-   s -= m * 60;
+    s = t;
+    h = (int)(s / 3600.0);
+    s -= h * 3600;
+    m = (int)(s / 60.0);
+    s -= m * 60;
 
-   str = eina_strbuf_new();
-   eina_strbuf_append_printf(str, "%02d:%02d:%02d", h, m, (int)s);
+    str = eina_strbuf_new();
+    eina_strbuf_append_printf(str, "%02d:%02d:%02d", h, m, (int)s);
 
-   elm_object_part_text_set(obj, part, eina_strbuf_string_get(str));
+    elm_object_part_text_set(obj, part, eina_strbuf_string_get(str));
 
-   eina_strbuf_free(str);
+    eina_strbuf_free(str);
 }
 
 static void
@@ -277,126 +277,126 @@ _slider_position_update_cb(void *data,
 static void 
 _emotion_position_update_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Enna_View_Player_Video_Data *priv = data;
-   Evas_Object *emotion;
-   Evas_Object *edje;
-   double v;
-   time_t timestamp;
-   struct tm *t;
-   Eina_Strbuf *str;
+    Enna_View_Player_Video_Data *priv = data;
+    Evas_Object *emotion;
+    Evas_Object *edje;
+    double v;
+    time_t timestamp;
+    struct tm *t;
+    Eina_Strbuf *str;
 
-   emotion = elm_video_emotion_get(priv->video);
+    emotion = elm_video_emotion_get(priv->video);
 
-   _update_time_part(priv->layout, "time_current.text", 
-                     emotion_object_position_get(emotion));
-   _update_time_part(priv->layout, "time_duration.text", 
-                     emotion_object_play_length_get(emotion));
+    _update_time_part(priv->layout, "time_current.text", 
+                      emotion_object_position_get(emotion));
+    _update_time_part(priv->layout, "time_duration.text", 
+                      emotion_object_play_length_get(emotion));
 
-   timestamp = time(NULL);
-   timestamp += emotion_object_play_length_get(emotion) -
-                emotion_object_position_get(emotion);
-   t = localtime(&timestamp);
-   str = eina_strbuf_new();
-   eina_strbuf_append_printf(str, "End at %02dh%02d", t->tm_hour, t->tm_min);
-   elm_object_part_text_set(priv->layout, "time_end_at.text", eina_strbuf_string_get(str));
-   eina_strbuf_free(str);
+    timestamp = time(NULL);
+    timestamp += emotion_object_play_length_get(emotion) -
+        emotion_object_position_get(emotion);
+    t = localtime(&timestamp);
+    str = eina_strbuf_new();
+    eina_strbuf_append_printf(str, "End at %02dh%02d", t->tm_hour, t->tm_min);
+    elm_object_part_text_set(priv->layout, "time_end_at.text", eina_strbuf_string_get(str));
+    eina_strbuf_free(str);
 
-   v = emotion_object_position_get(emotion) /
-       emotion_object_play_length_get(emotion);
+    v = emotion_object_position_get(emotion) /
+        emotion_object_play_length_get(emotion);
 
-   edje = elm_layout_edje_get(priv->layout);
-   edje_object_part_drag_value_set(edje, "time.slider", v, v);
+    edje = elm_layout_edje_get(priv->layout);
+    edje_object_part_drag_value_set(edje, "time.slider", v, v);
 }
 
 #if 0
 static void
 _item_file_name_get_cb(void *data, Ems_Node *node EINA_UNUSED, const char *value)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   if (value && value[0])
-     {
+    if (value && value[0])
+    {
         elm_object_part_text_set(priv->layout, "title.text", value);
-     }
-   else
-     {
+    }
+    else
+    {
         elm_object_part_text_set(priv->layout, "title.text", "Unknown");
         ERR("Cannot find a title for this file: :'(");
-     }
+    }
 }
 
 static void
 _item_name_get_cb(void *data, Ems_Node *node, const char *value)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   if (value && value[0])
-     {
+    if (value && value[0])
+    {
         elm_object_part_text_set(priv->layout, "title.text", value);
-     }
-   else
-     {
+    }
+    else
+    {
         ems_node_media_info_get(node, priv->media, "clean_name", _item_file_name_get_cb,
-                             NULL, NULL, priv);
-     }
+                                NULL, NULL, priv);
+    }
 }
 
 static void
 _item_poster_get_cb(void *data, Ems_Node *node EINA_UNUSED, const char *value)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   if (value)
-     {
+    if (value)
+    {
         priv->cover = elm_icon_add(priv->layout);
         elm_image_file_set(priv->cover, value, NULL);
         elm_image_preload_disabled_set(priv->cover, EINA_FALSE);
 
         elm_object_part_content_set(priv->layout, "cover.swallow", priv->cover);
         elm_object_signal_emit(priv->layout, "show,cover", "enna"); 
-     }
-   else
-     {
+    }
+    else
+    {
         elm_object_signal_emit(priv->layout, "hide,cover", "enna"); 
-     }
+    }
 }
 #endif
 
 static void
 _emotion_open_done_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   elm_object_signal_emit(priv->layout, "playing", "enna");
+    elm_object_signal_emit(priv->layout, "playing", "enna");
 }
 
 static void
 _emotion_playback_started_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   _set_osd_timer(priv, OSD_TIMER);
+    _set_osd_timer(priv, OSD_TIMER);
 }
 
 static void
 _emotion_playback_finished_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
    
-   evas_object_smart_callback_call(priv->layout, "playback_finished", NULL);
+    evas_object_smart_callback_call(priv->layout, "playback_finished", NULL);
 }
 
 static void
 _enna_view_del(void *data, Evas *e  EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
-   Enna_View_Player_Video_Data *priv = data;
+    Enna_View_Player_Video_Data *priv = data;
 
-   DBG("delete Enna_View_Player_Video_Data object (%p)", obj);
+    DBG("delete Enna_View_Player_Video_Data object (%p)", obj);
 
-   FREE_NULL_FUNC(evas_object_del, priv->video);
-   FREE_NULL_FUNC(evas_object_del, priv->cover);
-   FREE_NULL_FUNC(ecore_timer_del, priv->osd_timer);
-   FREE_NULL(priv->media);
+    FREE_NULL_FUNC(evas_object_del, priv->video);
+    FREE_NULL_FUNC(evas_object_del, priv->cover);
+    FREE_NULL_FUNC(ecore_timer_del, priv->osd_timer);
+    FREE_NULL(priv->media);
 }
 
 static void
@@ -464,41 +464,41 @@ _drag_cb(void *data,
 Evas_Object *
 enna_view_player_video_add(Evas_Object *parent)
 {
-   Evas_Object *layout;
-   Enna_View_Player_Video_Data *priv;
-   Evas_Object *emotion;
+    Evas_Object *layout;
+    Enna_View_Player_Video_Data *priv;
+    Evas_Object *emotion;
 
-   priv = calloc(1, sizeof(Enna_View_Player_Video_Data));
-   if (!priv)
-       return NULL;
+    priv = calloc(1, sizeof(Enna_View_Player_Video_Data));
+    if (!priv)
+        return NULL;
 
-   layout = elm_layout_add(parent);
-   elm_layout_file_set(layout, enna_config_theme_get(), "activity/layout/player/video");
-   priv->layout = layout;
+    layout = elm_layout_add(parent);
+    elm_layout_file_set(layout, enna_config_theme_get(), "activity/layout/player/video");
+    priv->layout = layout;
 
-   priv->video = elm_video_add(parent);
-   elm_object_part_content_set(layout, "video.swallow", priv->video);
-   elm_object_style_set(priv->video, "enna");
+    priv->video = elm_video_add(parent);
+    elm_object_part_content_set(layout, "video.swallow", priv->video);
+    elm_object_style_set(priv->video, "enna");
 
-   evas_object_data_set(layout, "Enna_View_Player_Video_Data", priv);
+    evas_object_data_set(layout, "Enna_View_Player_Video_Data", priv);
 
-   emotion = elm_video_emotion_get(priv->video);
-   evas_object_smart_callback_add(emotion, "position_update", _emotion_position_update_cb, priv);
-   evas_object_smart_callback_add(emotion, "open_done", _emotion_open_done_cb, priv);
-   evas_object_smart_callback_add(emotion, "playback_started", _emotion_playback_started_cb, priv);
-   evas_object_smart_callback_add(emotion, "playback_finished", _emotion_playback_finished_cb, priv);
-   evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_MOVE, _mouse_move_cb, priv);
-   evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_DOWN, _mouse_down_cb, priv);
-   evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_UP, _mouse_up_cb, priv);
+    emotion = elm_video_emotion_get(priv->video);
+    evas_object_smart_callback_add(emotion, "position_update", _emotion_position_update_cb, priv);
+    evas_object_smart_callback_add(emotion, "open_done", _emotion_open_done_cb, priv);
+    evas_object_smart_callback_add(emotion, "playback_started", _emotion_playback_started_cb, priv);
+    evas_object_smart_callback_add(emotion, "playback_finished", _emotion_playback_finished_cb, priv);
+    evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_MOVE, _mouse_move_cb, priv);
+    evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_DOWN, _mouse_down_cb, priv);
+    evas_object_event_callback_add(emotion, EVAS_CALLBACK_MOUSE_UP, _mouse_up_cb, priv);
 
-   elm_layout_signal_callback_add(layout, "drag", "*", _drag_cb, priv);
-   elm_layout_signal_callback_add(layout, "drag,stop", "*", _slider_position_update_cb, priv);
+    elm_layout_signal_callback_add(layout, "drag", "*", _drag_cb, priv);
+    elm_layout_signal_callback_add(layout, "drag,stop", "*", _slider_position_update_cb, priv);
 
-   emotion_object_init(emotion, mp_cfg.engine);
+    emotion_object_init(emotion, mp_cfg.engine);
 
-   evas_object_event_callback_add(layout, EVAS_CALLBACK_DEL, _enna_view_del, priv);
+    evas_object_event_callback_add(layout, EVAS_CALLBACK_DEL, _enna_view_del, priv);
 
-   return layout;
+    return layout;
 }
 
 void enna_view_player_video_uri_set(Evas_Object *o, Enna_File *f)
@@ -507,71 +507,71 @@ void enna_view_player_video_uri_set(Evas_Object *o, Enna_File *f)
     const char *title;
     int prefix = 0;
     
-   PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
+    PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
 
-   priv->media = strdup(f->mrl);
+    priv->media = strdup(f->mrl);
 
-   DBG("Start video player with item: %s", f->mrl);
+    DBG("Start video player with item: %s", f->mrl);
 
-   if (!strncmp(f->mrl, "file://", 7))
-       prefix = 7;
-   elm_video_file_set(priv->video, f->mrl + prefix);
+    if (!strncmp(f->mrl, "file://", 7))
+        prefix = 7;
+    elm_video_file_set(priv->video, f->mrl + prefix);
 
-   title = enna_file_meta_get(f, "title");
-   if (title)
-   {
-       elm_object_part_text_set(priv->layout, "title.text", title);
-       eina_stringshare_del(title);
-   }   
-   else
-   {
-       title = ecore_file_file_get(f->mrl);
-       elm_object_part_text_set(priv->layout, "title.text", title);
-   }
+    title = enna_file_meta_get(f, "title");
+    if (title)
+    {
+        elm_object_part_text_set(priv->layout, "title.text", title);
+        eina_stringshare_del(title);
+    }   
+    else
+    {
+        title = ecore_file_file_get(f->mrl);
+        elm_object_part_text_set(priv->layout, "title.text", title);
+    }
 
-   cover = enna_file_meta_get(f, "cover");
-   if (cover)
-   {
-       priv->cover = elm_icon_add(priv->layout);
-       printf("cover : %s\n", enna_file_meta_get(f, "cover"));
+    cover = enna_file_meta_get(f, "cover");
+    if (cover)
+    {
+        priv->cover = elm_icon_add(priv->layout);
+        printf("cover : %s\n", enna_file_meta_get(f, "cover"));
        
-       elm_image_file_set(priv->cover, cover,  NULL);
-       elm_image_preload_disabled_set(priv->cover, EINA_FALSE);
+        elm_image_file_set(priv->cover, cover,  NULL);
+        elm_image_preload_disabled_set(priv->cover, EINA_FALSE);
        
-       elm_object_part_content_set(priv->layout, "cover.swallow", priv->cover);
-       elm_object_signal_emit(priv->layout, "show,cover", "enna"); 
-   }
-   else
-   {
-       elm_object_signal_emit(priv->layout, "hide,cover", "enna"); 
-   }
+        elm_object_part_content_set(priv->layout, "cover.swallow", priv->cover);
+        elm_object_signal_emit(priv->layout, "show,cover", "enna"); 
+    }
+    else
+    {
+        elm_object_signal_emit(priv->layout, "hide,cover", "enna"); 
+    }
 }
 
 void enna_view_player_video_play(Evas_Object *o)
 {
-   PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
+    PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
 
-   if (!elm_video_is_playing_get(priv->video))
-       elm_video_play(priv->video);
-   else
-       elm_video_pause(priv->video);
+    if (!elm_video_is_playing_get(priv->video))
+        elm_video_play(priv->video);
+    else
+        elm_video_pause(priv->video);
 }
 
 void enna_view_player_video_pause(Evas_Object *o)
 {
-   PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
+    PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
 
-   if (!elm_video_is_playing_get(priv->video))
-       elm_video_play(priv->video);
-   else
-       elm_video_pause(priv->video);
+    if (!elm_video_is_playing_get(priv->video))
+        elm_video_play(priv->video);
+    else
+        elm_video_pause(priv->video);
 }
 
 void enna_view_player_video_stop(Evas_Object *o)
 {
-   PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
+    PRIV_GET_OR_RETURN(o, Enna_View_Player_Video_Data, priv);
 
-   elm_video_stop(priv->video);
+    elm_video_stop(priv->video);
 }
 
 void enna_videoplayer_obj_cfg_register(void)
@@ -612,9 +612,9 @@ void enna_view_video_player_seek(Evas_Object *o, double time)
 
     v = pos / emotion_object_play_length_get(emotion);
 
-   edje = elm_layout_edje_get(priv->layout);
-   edje_object_part_drag_value_set(edje, "time.slider", v, v);
+    edje = elm_layout_edje_get(priv->layout);
+    edje_object_part_drag_value_set(edje, "time.slider", v, v);
 
-   emotion_object_position_set(emotion, pos);
+    emotion_object_position_set(emotion, pos);
 
 }
